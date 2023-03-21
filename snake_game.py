fc022d80430b0b5389d01a07f2c334da08ada349
Tymotesuz Broda <tymoteusz.91100@gmail.com>
2023-03-21 14:23:15 +0100

    added word interpretation of colors that will be used

--- snake_game.py
+++ snake_game.py
@@ -7,6 +7,33 @@ WINDOW_HEIGHT = 600
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Snake!")
 
+#FPS and CLOCK
+FPS = 20
+clock = pygame.time.Clock()
+#game values
+SNAKE_SIZE = 20
+headX = WINDOW_WIDTH//2
+headY = WINDOW_HEIGHT//2 + 100
+
+snakeDx = 0
+snakeDy = 0
+
+score = 0
+
+
+#colors
+GREEN = (0,255,0)
+DARKGREEN = (10,50,10)
+RED = (255,0,0)
+DARKRED = (150,0,0)
+WHITE = (255,255,255)
+#fonts
+
+#text
+
+#sounds
+
+#images
 #=============================GAME LOOP===============================
 running = True
 while running:
@@ -14,5 +41,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    window.fill((255,255,255))
+    window.fill(WHITE)
     pygame.display.update()
+    clock.tick(FPS)
+pygame.quit()
